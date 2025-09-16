1792e0008754b318ee543f985cec58f2d638dfd5
kaushik269 <kaushikjindal157@gmail.com>
2025-09-16 12:10:47 +0530

    DSA Lab Assignment 3 [1024030431].cpp

--- DSA Lab Assignment 3 [1024030431].cpp
+++ DSA Lab Assignment 3 [1024030431].cpp
@@ -149,4 +149,231 @@ int main(){
 }*/
 
 
+//ques 3 -> balanced brackets
+
+/*#include<iostream>
+using namespace std;
+#define MAX 100
+
+class Stack {
+    char arr[MAX];
+    int top;
+
+public:
+    Stack() { 
+	top = -1;
+	}
+	
+    bool isEmpty() { 
+	return top == -1;
+	}
+
+    bool isFull() { 
+	return top == MAX - 1; 
+	}
+
+    void push(char val) {
+        if (isFull()) {
+            cout<<"Stack Overflow"<<endl;
+            return;
+        }
+        arr[++top] = val;
+    }
+
+    char pop() {
+        if (isEmpty()) {
+            return -1; // return null char if empty
+        }
+        return arr[top--];
+    }
+
+    char peek() {
+        if (isEmpty()) return -1;
+        return arr[top];
+    }
+};
+
+bool isBalanced(string expr) {
+    Stack s;
+
+    for (int i = 0; i < expr.length(); i++) {
+        char ch = expr[i];
+
+        // If opening bracket, push to stack
+        if (ch == '(' || ch == '{' || ch == '[') {
+            s.push(ch);
+        }
+        // If closing bracket
+        else if (ch == ')' || ch == '}' || ch == ']') {
+            if (s.isEmpty()) return false; // nothing to match
+
+            char top = s.pop();
+
+            // check correct matching
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+
+    // expression is balanced only if stack is empty
+    return s.isEmpty();
+}
+
+int main() {
+    string expr;
+    cout<<"Enter an expression : ";
+    cin>>expr;
+
+    if (isBalanced(expr))
+        cout<<"Balanced Parentheses"<<endl;
+    else
+        cout<<"Not Balanced"<<endl;
+    return 0;
+}*/
+
+
+//ques 4 -> infix to postfix 
+
+/*#include <iostream>
+using namespace std;
+#define MAX 100
+
+class Stack {
+    char arr[MAX];
+    int top;
+
+public:
+    Stack() { 
+	top = -1; 
+	}
+
+    bool isEmpty() { 
+	return top == -1; 
+	}
+
+    bool isFull() {
+	return top == MAX - 1; 
+	}
+
+    void push(char val) {
+        if (isFull()) {
+            cout << "Stack Overflow\n";
+            return;
+        }
+        arr[++top] = val;
+    }
+
+    char pop() {
+        if (isEmpty()) return '\0';
+        return arr[top--];
+    }
+
+    char peek() {
+        if (isEmpty()) return '\0';
+        return arr[top];
+    }
+};
+
+// precedence of operators
+int precedence(char op) {
+    if (op == '^') return 3;
+    else if (op == '*' || op == '/') return 2;
+    else if (op == '+' || op == '-') return 1;
+    else return -1;
+}
+
+// check if operand (letter or digit)
+bool isOperand(char ch) {
+    if ((ch >= 'a' && ch <= 'z') || 
+        (ch >= 'A' && ch <= 'Z') || 
+        (ch >= '0' && ch <= '9'))
+        return true;
+    return false;
+}
+
+string infixToPostfix(string expr) {
+    Stack s;
+    string result = "";
+
+    for (int i = 0; i < expr.length(); i++) {
+        char ch = expr[i];
+
+        if (isOperand(ch)) {
+            result += ch;  // operand ? output
+        }
+        else if (ch == '(') {
+            s.push(ch);
+        }
+        else if (ch == ')') {
+            while (!s.isEmpty() && s.peek() != '(') {
+                result += s.pop();
+            }
+            s.pop(); // remove '('
+        }
+        else { // operator
+            while (!s.isEmpty() && precedence(ch) <= precedence(s.peek())) {
+                result += s.pop();
+            }
+            s.push(ch);
+        }
+    }
+
+    // pop remaining operators
+    while (!s.isEmpty()) {
+        result += s.pop();
+    }
+
+    return result;
+}
+
+int main() {
+    string expr;
+    cout<<"Enter infix expression : ";
+    cin>>expr;
+
+    cout<<"Postfix expression : "<<infixToPostfix(expr)<<endl;
+    return 0;
+}*/
+
+//ques 5 -> postfix eval
+
+/*#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+int solve(int val1, int val2, int ch){
+    if(ch=='+') return val1+val2;
+    else if(ch=='-') return val1-val2;
+    else if(ch=='*') return val1*val2;
+    else return val1/val2;
+}
+int main(){
+    string s = "26+4*8/3-"; // postfix expression
+    // we need only one stack for values
+    stack<int>val;
+    for(int i=0;i<s.length();i++){
+        // check if s[i] is digit (0-9)
+        if(s[i]>=48 && s[i]<=57){ // digit
+            val.push(s[i]-48);
+        }
+        else{ // s[i] it is -> *,/,+,-
+            // work
+            int val2 = val.top();
+            val.pop();
+            int val1 = val.top();
+            val.pop();
+            int ans = solve(val1,val2,s[i]);
+            val.push(ans);
+        }
+    }
+    cout<<"26+4*8/3- gives : "<<val.top()<<endl; // 1
+    return 0;
+}*/
+
+
+
  
+
